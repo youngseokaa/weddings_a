2718aa7a5f524a1b7b928df5a2eb7b4c6f124265
youngseokaa <jennykim6890@naver.com>
2023-01-17 03:51:12 +0900

    커밋이영

--- app.py
+++ app.py
@@ -34,18 +34,53 @@ def weddingCancel():
     num_receive = request.form['num_gives']
     db.wedding.delete_one({'num': int(num_receive)})
     return jsonify({'msg':'취소 완료!'})
-@app.route('/modify')
+@app.route('/modify', methods=["GET"])
 def modify():
     return render_template('weddingModify.html')
+@app.route('/showInvitation', methods=["GET"])
+def modify_showInvitation():
+    wedding_list = list(db.wedding.find({}, {'_id': False}))
+    return jsonify({'wedding': wedding_list})
 
 @app.route('/write', methods=["GET"])
 def write():
     return render_template('weddingWrite.html')
 
-@app.route('/write/check', methods=["GET"])
+@app.route('/modify/check', methods=["GET"])
 def signUpGet():
     weddinglist = list(db.wedding.find({}, {'_id': False}))
     return jsonify({'wedding': weddinglist})
+
+@app.route("/modify/cancel", methods=["POST"])
+def bucket_cancel():
+    num_receive = request.form['num_give']
+    manName_receive = request.form['manName_give']
+    girlName_receive = request.form['girlName_give']
+    manTel_receive = request.form['manTel_give']
+    girlTel_receive = request.form['girlTel_give']
+    weddingDay_receive = request.form['weddingDay_give']
+    weddingPlace_receive = request.form['weddingPlace_give']
+    comment_receive = request.form['comment_give']
+    manMom_receive = request.form['manMom_give']
+    manDad_receive = request.form['manDad_give']
+    girlDad_receive = request.form['girlDad_give']
+    manAccount_receive = request.form['manAccount_give']
+    girlAccount_receive = request.form['girlAccount_give']
+    db.bucket.update_one({'num': int(num_receive)}, {'$set': {'manName_give': manName_receive}})
+    db.bucket.update_one({'num': int(num_receive)}, {'$set': {'girlName_give': girlName_receive}})
+    db.bucket.update_one({'num': int(num_receive)}, {'$set': {'manTel_give': manTel_receive}})
+    db.bucket.update_one({'num': int(num_receive)}, {'$set': {'girlTel_give': girlTel_receive}})
+    db.bucket.update_one({'num': int(num_receive)}, {'$set': {'weddingDay_give': weddingDay_receive}})
+    db.bucket.update_one({'num': int(num_receive)}, {'$set': {'weddingPlace_give':weddingPlace_receive}})
+    db.bucket.update_one({'num': int(num_receive)}, {'$set': {'comment_give': comment_receive}})
+    db.bucket.update_one({'num': int(num_receive)}, {'$set': {'manMom_give': manMom_receive}})
+    db.bucket.update_one({'num': int(num_receive)}, {'$set': {'manDad_give': manDad_receive}})
+    db.bucket.update_one({'num': int(num_receive)}, {'$set': {'girlDad_give': girlDad_receive}})
+    db.bucket.update_one({'num': int(num_receive)}, {'$set': {'manAccount_give': manAccount_receive}})
+    db.bucket.update_one({'num': int(num_receive)}, {'$set': {'manAccount_give': girlAccount_receive}})
+    db.bucket.update_one({'num': int(num_receive)}, {'$set': {'girlAccount_give': 0}})
+    return jsonify({'msg': '수정 완료!'})
+
 
 @app.route('/write_invitation', methods=["POST"])
 def write_invitation():
